cccf2591d51efadacc9038914cd93f7d531e9f22
Fareeha09 <ftabassum@stuy.edu>
2018-12-14 04:02:10 -0500

    added struct

--- control.c
+++ control.c
@@ -7,9 +7,18 @@
 #include <sys/sem.h>
 #include <sys/ipc.h>
 #include <sys/types.h>
+#include <errno.h>
 
-//add struct sembuf
-//add union semun
+/*struct sembuf is already included i think from
+  the sample codes i looked at online
+  */
+
+union semun{
+	int val; //value for SETVAL
+	struct semid_ds *buf; //buffer for IPC_STAT & IPC_SET
+	unsigned short *array; //array for GETALL/SETALL
+	struct seminfo *__buff; //buffer for IPC_INFO(linux specific) 
+};
 
 int main(int argc, char* argv[]){
   if (argc == 1){
@@ -21,18 +30,22 @@ int main(int argc, char* argv[]){
     return 0;
   }
 
-  key_t sem_key = ftok(".",'S');
-  key_t mem_key = ftok(".",'R');
+  key_t sem_key = ftok(".", 'S');
+  key_t mem_key = ftok(".", 'R');
 
-  if (strcmp(argv[1],"-c") == 0){
-    shmget(mem_key,127,IPC_CREAT | 0644); //creates shared mem
-    int semID = semget(sem_key,1,IPC_CREAT | 0644); //creates semaphore
-    open("story", IPC_CREAT | O_TRUNC, 0644); //creates file
-    union semun data;
+  if (!strcmp(argv[1],"-c")){
+	shmget(mem_key,127,IPC_CREAT | 0644); //creates shared mem
+    int semID = semget(sem_key, 1, IPC_CREAT | 0644); //creates semaphore
+    open("story", O_CREAT | O_RDWR | O_TRUNC);
+	//open("story", IPC_CREAT | O_TRUNC, 0644); //creates file
+	union semun data;
     data.val = 1;
     semctl(semID,0,SETVAL,data);
+	
+	//printf("semID: %d\n", semID);
+	if (semID == -1){ printf("error: %s", strerror(errno));}
   }
-  if (strcmp(argv[1],"-r") == 0){
+  if (!strcmp(argv[1],"-r")){
     int semID = semget(sem_key,1,0); //accesses semaphore
     struct sembuf down;
     down.sem_op = -1;
@@ -51,7 +64,7 @@ int main(int argc, char* argv[]){
     remove("story"); //removes file
   }
 
-  if (strcmp(argv[1],"-v") == 0){
+  if (!strcmp(argv[1],"-v")){
     int fd = open("story", O_RDONLY);
     char * story = calloc(10000,1);
     read(fd,story,10000);
